af7ed71a41fc5c7dd79cb11b9663a31bd0f8bbfd
sudheervgts <sudheer@vishgyana.com>
2024-01-12 23:06:44 +0530

    ....

--- dj_dynamic_templates/admin.py
+++ dj_dynamic_templates/admin.py
@@ -1,29 +1,155 @@
-from django.contrib import admin
-
-from .models import *
-
-
-class BaseAdmin(admin.ModelAdmin):
-    readonly_fields = ['created_at', 'created_by']
+import os
+from django.utils.html import format_html
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.admin import UserAdmin
+from .forms import *
 
 
 @admin.register(DjDynamicTemplateCategory)
-class DjDynamicTemplateCategoryAdmin(BaseAdmin):
+class DjDynamicTemplateCategoryAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'app', 'name', 'last_updated_at', 'last_updated_by']
+    list_per_page = 20
+    list_filter = ['app', 'created_by', 'last_updated_by', 'last_updated_at']
+    list_select_related = True
+    list_display_links = ['id', 'app', 'name']
+    search_fields = ['name']
+    search_help_text = 'Search with Category Name'
+    date_hierarchy = 'created_at'
+
+    change_form_template = 'category_change_form.html'
+    form = DjDynamicTemplateCategoryForm
+    actions = ['create_directory', 'delete_directory']
+    readonly_fields = ['created_by', 'created_at', 'last_updated_at', 'last_updated_by']
+
+    fieldsets = [
+        ('Basic Information', {
+            'fields': ['app', 'name', 'description'],
+            'classes': ['wide']
+        }),
+        ('Creation Details', {
+            'fields': ['created_at', 'created_by'],
+            'classes': ['collapse', 'wide']
+        }),
+        ('Update Details', {
+            'fields': ['last_updated_at', 'last_updated_by'],
+            'classes': ['collapse', 'wide']
+        }),
+    ]
+
+    @staticmethod
+    def is_directory_exists(obj):
+        if obj.is_directory_exists:
+            return format_html("""<img src="/static/admin/img/icon-yes.svg" alt="True">""")
+        else:
+            return format_html("""<img src="/static/admin/img/icon-no.svg" alt="True">""")
+
+    def get_list_display(self, request):
+        fields = self.list_display.copy()
+        if request.user.has_perm('dj_dynamic_templates.can_view_directory_status'):
+            fields.append('is_directory_exists')
+        return fields
+
+    def get_fieldsets(self, request, obj=None):
+        if obj is None:
+            return [self.fieldsets[0]]
+        fieldsets = self.fieldsets.copy()
+        if request.user.has_perm('dj_dynamic_templates.can_view_directory_status'):
+            fieldsets.append(('Directory Details', {'fields': ['is_directory_exists'], 'classes': ['wide']}))
+        if request.user.has_perm('dj_dynamic_templates.can_view_files_in_directory'):
+            if fieldsets[-1][0] == 'Directory Details':
+                fieldsets[-1][1]['fields'].append('files_in_directory')
+            else:
+                fieldsets.append(('Directory Details', {'fields': ['files_in_directory'], 'classes': ['wide']}))
+        return fieldsets
+
+    def get_actions(self, request):
+        actions = super(DjDynamicTemplateCategoryAdmin, self).get_actions(request)
+        if not request.user.has_perm('dj_dynamic_templates.can_create_directory'):
+            del actions['create_directory']
+        if not request.user.has_perm('dj_dynamic_templates.can_delete_directory'):
+            del actions['delete_directory']
+        return actions
 
     def get_readonly_fields(self, request, obj=None):
+        fields = self.readonly_fields.copy()
         if obj:
-            return self.readonly_fields + ['last_updated', 'last_updated_by']
+            if request.user.has_perm('dj_dynamic_templates.can_view_directory_status'):
+                fields.append('is_directory_exists')
+            if request.user.has_perm('dj_dynamic_templates.can_view_files_in_directory'):
+                fields.append('files_in_directory')
+            return fields
         else:
             return []
+
+    @staticmethod
+    def files_in_directory(obj):
+        return format_html("<ol>" + "".join(['<li>{file_name}</li>'.format(file_name=file) for file in obj.files_in_dir]) + "</ol>")
+
+    @admin.action(description='Generate Category Folders for selected Records')
+    def create_directory(self, request, queryset):
+        for obj in queryset:
+            if obj.make_directory():
+                self.message_user(request, f"Directory '{obj.name}' has been successfully created in the 'templates' directory of the '{obj.app}' app.", messages.SUCCESS)
+            else:
+                self.message_user(request, f"Directory '{obj.name}' already exists in the 'templates' directory of the '{obj.app}' app.", messages.ERROR)
+
+    @admin.action(description='Delete Directory for selected Records')
+    def delete_directory(self, request, queryset):
+        for obj in queryset:
+            if obj.remove_directory():
+                self.message_user(request, f"Directory '{obj.name}' has been successfully deleted in the 'templates' directory of the '{obj.app}' app.", messages.SUCCESS)
+            else:
+                self.message_user(request, f"Directory '{obj.name}' does not exist in the 'templates' directory of the '{obj.app}' app.", messages.ERROR)
+
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
+        if '_sync_dir' in request.POST:
+            self.create_directory(request, [self.get_object(request, object_id)])
+            return redirect('.')
+        elif '_rmdir' in request.POST:
+            self.delete_directory(request, [self.get_object(request, object_id)])
+            return redirect('.')
+        return super().changeform_view(request, object_id, form_url, extra_context)
 
     def save_model(self, request, obj, form, change):
         if change:
             obj.last_updated_by = request.user
+            old_obj = self.model.objects.get(pk=obj.pk)
         else:
-            obj.created_by = request.user
+            obj.created_by = obj.last_updated_by = request.user
+            if '_make_dir' in request.POST:
+                self.create_directory(request, [obj])
         obj.save()
+        if change and old_obj.directory_path != obj.directory_path and '_make_dir' in request.POST:
+            try:
+                os.rename(old_obj.directory_path, obj.directory_path)
+                self.message_user(request, f"Directory path updated: Changed from '{old_obj.directory_path}' to '{obj.directory_path}'.", messages.SUCCESS)
+            except FileExistsError:
+                self.message_user(request, f"Directory '{obj.name}' already exists in the 'templates' directory of the '{obj.app}' app.", messages.ERROR)
+            except FileNotFoundError:
+                if old_obj.app != obj.app:
+                    self.create_directory(request, [obj])
+                else:
+                    self.message_user(request, f"Directory '{old_obj.name}' does not exist in the 'templates' directory of the '{old_obj.app}' app.", messages.ERROR)
+
+    def delete_model(self, request, obj):
+        obj.remove_directory()
+        obj.delete()
 
 
 @admin.register(DjDynamicTemplate)
 class DjDynamicTemplateAdmin(admin.ModelAdmin):
-    pass
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields
+        else:
+            return []
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.save()
--- dj_dynamic_templates/models.py
+++ dj_dynamic_templates/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.apps import apps
-
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+import os
+import shutil
 from django.db import models
 from django.db.models.functions import Now
 
@@ -8,47 +11,167 @@ from django.db.models.functions import Now
 
 class BaseModel(models.Model):
 
-    created_at = models.DateTimeField(db_default=Now(), editable=False)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, editable=False)
+    help_texts = {
+        'created_at': _(''),
+        'created_by': _('')
+    }
+
+    created_at = models.DateTimeField(
+        verbose_name=_('Created at'), db_default=Now(), editable=False, help_text=help_texts['created_at']
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, editable=False,
+        verbose_name=_('Created by'), help_text=help_texts['created_by']
+    )
 
 
     class Meta:
         abstract = True
 
 
-class DjDynamicTemplateCategory(BaseModel):
+class DjDynamicTemplateCategory(models.Model):
 
-    app_name = models.CharField(max_length=100, help_text="In this App, Directory will be created within templates folder")
-    category_name = models.CharField(max_length=100, help_text="Will use this name as an Directory name")
-    description = models.TextField(null=True, blank=True, help_text="Description of the category")
+    app = models.CharField(
+        verbose_name=_('App'), max_length=100,
+        help_text=_("Select the Django app where a directory will be created within the 'templates' folder. "
+                    "This helps organize templates under the selected app.")
+    )
+    name = models.CharField(
+        verbose_name=_('Category Name'), max_length=100,
+        help_text=_("Enter a unique name for the directory that will be created. "
+                    "This name will be used as the directory name within the 'templates' folder for the selected app.")
+    )
 
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
-    last_updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, editable=False, related_name='dj_dynamic_template_category_last_updated_by')
+    description = models.TextField(
+        verbose_name=_('Description'), null=True, blank=True,
+        help_text=_("Provide a brief description of the category. "
+                    "This description is optional and can help document the purpose or content of the templates within the created directory")
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name=_('Created at'), db_default=Now(), editable=False,
+        help_text=_("The date and time when this template category was created. "
+                    "This field is automatically set to the current date and time when the record is first created.")
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, editable=False, verbose_name=_('Created by'),
+        help_text=_("The user who created this template category. "
+                    "This field is automatically populated with the user who initially created the record.")
+    )
+
+    last_updated_at = models.DateTimeField(
+        verbose_name=_('Last Updated at'), auto_now=True, editable=False,
+        help_text=_("The date and time when this template category was last updated. "
+                    "This field is automatically updated every time the record is modified.")
+    )
+
+    last_updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, editable=False,
+        related_name='dj_dynamic_template_category_last_updated_by', verbose_name=_('Last Updated by'),
+        help_text=_("The user who last updated this template category. "
+                    "This field is automatically populated with the user making the modification.")
+    )
 
     def __str__(self):
-        return f'{self.app_name} - {self.category_name}'
+        return f'{self.app} - {self.name}'
+
+    @cached_property
+    def directory_path(self):
+        return os.path.join(settings.BASE_DIR, self.app, 'templates', self.name)
+
+    @cached_property
+    def is_directory_exists(self):
+        return os.path.exists(self.directory_path)
+
+    @cached_property
+    def files_in_dir(self):
+        if self.is_directory_exists:
+            return os.listdir(self.directory_path)
+        else:
+            return list()
+
+    def make_directory(self):
+        if not self.is_directory_exists:
+            os.makedirs(self.directory_path, exist_ok=False)
+            return True
+        return False
+
+    def remove_directory(self):
+        if self.is_directory_exists:
+            shutil.rmtree(self.directory_path, ignore_errors=True)
+            return True
+        else:
+            return False
 
 
     class Meta:
         managed = apps.is_installed("dj_dynamic_templates")
-        ordering = ('app_name', )
-        unique_together = ('app_name', 'category_name')
+        ordering = ('app', )
+        unique_together = ('app', 'name')
+        permissions = (
+            ('can_create_directory', 'Can Create Directory'),
+            ('can_delete_directory', 'Can Delete Directory'),
+            ('can_view_files_in_directory', 'Can View Files in Directory'),
+            ('can_view_directory_status', 'Can View Directory Status')
+        )
         db_table = 'dj_dynamic_template_category'
-        verbose_name = db_table.replace("_", " ").title()
+        verbose_name = _(db_table.replace("_", " ").title())
         verbose_name_plural = verbose_name.replace("Category", "Categories")
 
 
 class DjDynamicTemplate(BaseModel):
 
-    category = models.ForeignKey(DjDynamicTemplateCategory, on_delete=models.CASCADE, help_text="Select the category where you want to save the template")
-    template_name = models.CharField(max_length=100, help_text="Entered name will treated as template file name, A template file is created with this file name")
-    content = models.TextField(null=True, blank=True, help_text="Insert the content of the template")
+    help_texts = {
+        'created_at': _("The date and time when this template was created. "
+                        "This field is automatically set to the current date and time when the record is first created."),
+        'created_by': _("The user who created this template. "
+                        "This field is automatically populated with the user who initially created the record.")
+    }
 
-    is_active = models.BooleanField(default=True, help_text="This field determines the current status of the template")
-    parent_obj = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, editable=False)
+    category = models.ForeignKey(
+        DjDynamicTemplateCategory, on_delete=models.CASCADE, verbose_name=_('Category'),
+        help_text=_("Select the category where you want to save the template. "
+                    "This categorization helps organize and manage your templates efficiently.")
+    )
+    template_name = models.CharField(
+        verbose_name=_('Template Name'), max_length=100,
+        help_text=_("Enter a name for the template. This name will be treated as the template file name. "
+                    "A template file(.html) with this name will be created.")
+    )
+    content = models.TextField(
+        verbose_name=_('Content'), null=True, blank=True,
+        help_text=_("Insert the content of the template. "
+                    "This field allows you to define the content of your template, which can include HTML and inline CSS")
+    )
+
+    template_is_active = models.BooleanField(
+        verbose_name=_("Template is Active"), default=True,
+        help_text=_("This field determines the current status of the template. "
+                    "If set to 'True', the template is considered active. "
+                    "If set to 'False', it is considered inactive. Inactive templates will not clone the templates")
+    )
+
+    revision_of = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True, editable=False, verbose_name=_("Previous Template Revision"),
+        help_text=_("This field is used when editing a template. "
+                    "Create a new record to represent the updated version of the template and link it to the previous version using this field. "
+                    "It indicates the previous template revision that this record is based on.")
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name=_('Created at'), db_default=Now(), editable=False,
+        help_text=_("The date and time when this template category was created. "
+                    "This field is automatically set to the current date and time when the record is first created.")
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, editable=False, verbose_name=_('Created by'),
+        help_text=_("The user who created this template category. "
+                    "This field is automatically populated with the user who initially created the record.")
+    )
 
     def __str__(self):
-        return f'{self.category.app_name} - {self.category.category_name} - {self.template_name}'
+        return f'{self.category.app} - {self.category.name} - {self.template_name}'
 
 
     class Meta:
@@ -57,12 +180,13 @@ class DjDynamicTemplate(BaseModel):
         constraints = [
             models.UniqueConstraint(
                 fields=('template_name', 'category'),
-                condition=models.Q(is_active=True),
+                condition=models.Q(template_is_active=True),
                 name='unique_category_template',
-                violation_error_code=1,
-                violation_error_message='They is an already an template in this category'
+                violation_error_code='DUPLICATE_TEMPLATE_IN_APP_CATEGORY',
+                violation_error_message=_('A template with the same name already exists in this category. '
+                                          'Please choose a different name or deactivate the existing template.')
             )
         ]
         db_table = 'dj_dynamic_template'
-        verbose_name = db_table.replace("_", " ").title()
+        verbose_name = _(db_table.replace("_", " ").title())
         verbose_name_plural = verbose_name.replace("Template", "Templates")
